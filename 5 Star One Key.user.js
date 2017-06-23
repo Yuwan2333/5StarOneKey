3e71ea8f2a3466dd053eeb2377c957ff9cc71967
Yuwan2333 <1129164045@qq.com>
2017-06-23 23:48:23 +0800

    Update 5 Star One Key.user.js

--- 5 Star One Key.user.js
+++ 5 Star One Key.user.js
@@ -15,7 +15,7 @@
     var target1 = document.getElementById("submitDiv");
     
     var five_star = document.createElement("button");
-    var textnode = document.createTextNode("Five Star");
+    var textnode = document.createTextNode("五星！");
     five_star.className = "button big-submit-button";
     five_star.appendChild(textnode);
     target1.appendChild(five_star);
@@ -49,6 +49,19 @@
     target1.appendChild(_553355);
     _553355.onclick = function(){
         document.querySelector("#AnswersController > form > div:nth-child(1) > div:nth-child(1) > div.btn-group > button:nth-child(5)").click();
+        document.querySelector("#AnswersController > form > div:nth-child(1) > div.col-xs-12.col-sm-4.pull-right.text-center > div:nth-child(5) > button:nth-child(5)").click();
+        document.querySelector("#AnswersController > form > div:nth-child(1) > div.col-xs-12.col-sm-4.pull-right.text-center > div:nth-child(10) > button:nth-child(3)").click();
+        document.querySelector("#AnswersController > form > div:nth-child(1) > div.col-xs-12.col-sm-4.pull-right.text-center > div:nth-child(15) > button:nth-child(3)").click();
+        document.querySelector("#AnswersController > form > div:nth-child(2) > div:nth-child(1) > div:nth-child(5) > button:nth-child(5)").click();
+        document.querySelector("#AnswersController > form > div:nth-child(2) > div:nth-child(1) > div:nth-child(10) > button:nth-child(5)").click();
+    };
+        var _353355 = document.createElement("button");
+    var textnode = document.createTextNode("353355");
+    _533355.className = "button big-submit-button";
+    _533355.appendChild(textnode);
+    target1.appendChild(_533355);
+    _533355.onclick = function(){
+        document.querySelector("#AnswersController > form > div:nth-child(1) > div:nth-child(1) > div.btn-group > button:nth-child(3)").click();
         document.querySelector("#AnswersController > form > div:nth-child(1) > div.col-xs-12.col-sm-4.pull-right.text-center > div:nth-child(5) > button:nth-child(5)").click();
         document.querySelector("#AnswersController > form > div:nth-child(1) > div.col-xs-12.col-sm-4.pull-right.text-center > div:nth-child(10) > button:nth-child(3)").click();
         document.querySelector("#AnswersController > form > div:nth-child(1) > div.col-xs-12.col-sm-4.pull-right.text-center > div:nth-child(15) > button:nth-child(3)").click();
